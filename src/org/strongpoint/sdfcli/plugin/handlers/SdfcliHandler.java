655deb55e9551bf3e7d0756b93273f623c26cb03
Sherwen Sol Dizon <ssoldizon@lingotek.com>
2018-09-01 13:32:24 +0800

    Initial commit for Strongpoint SDFCLI

---
+++ src/org/strongpoint/sdfcli/plugin/handlers/SdfcliHandler.java
@@ -0,0 +1,44 @@
+package org.strongpoint.sdfcli.plugin.handlers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class SdfcliHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Strongpoint-sdfcli",
+				"Hello, Eclipse world");
+        try {
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec("pwd");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line=null;
+
+            while((line=input.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitVal = process.waitFor();
+            System.out.println("Exited with error code "+exitVal);
+
+        } catch(Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
+		return null;
+	}
+}
