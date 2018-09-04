87fe3669ce5b63a3525d7bf78c4f27d6e40545dc
sherwendizon <ssadizon@gmail.com>
2018-09-04 22:42:32 +0800

    Added static test data in impact analysis

--- src/org/strongpoint/sdfcli/plugin/handlers/SdfcliImpactAnalysisHandler.java
+++ src/org/strongpoint/sdfcli/plugin/handlers/SdfcliImpactAnalysisHandler.java
@@ -22,7 +22,19 @@ public class SdfcliImpactAnalysisHandler extends AbstractHandler {
 		MessageDialog.openInformation(
 				window.getShell(),
 				"Strongpoint Impact Analysis",
-				"No significant impact to show!");
+				"No significant impact to show!safe: [\n" + 
+				"		{scriptid: 'customscript_flo_trigger', name: 'Strongpoint Trigger Script'},\n" + 
+				"		{scriptid: 'customsearch_flo_unused', name: 'Strongpoint Unused Search'},\n" + 
+				"		{scriptid: 'customscript123', name: 'Test Script'},\n" + 
+				"		{scriptid: 'customsearch1122', name: 'Test Search'}\n" + 
+				"	],\n" + 
+				"	notSafe: [\n" + 
+				"		{scriptid: 'customscript_flo_notsafetrigger', name: 'Strongpoint Not Safe Trigger Script', warning: 'RECENTLY USED', impacted: [{scriptid: 'customrecord1', name: 'Record 1'}, {scriptid: 'customrecord2', name: 'Record 2'}, {scriptid: 'customrecord3', name: 'Record 3'}]},\n" + 
+				"		{scriptid: 'customsearch_flo_testsearch', name: 'Strongpoint Test Search', warning: 'RECENTLY USED', impacted: [{scriptid: 'customrecord1', name: 'Record 1'}, {scriptid: 'customrecord2', name: 'Record 2'}, {scriptid: 'customrecord3', name: 'Record 3'}]}\n" + 
+				"	],\n" + 
+				"	notActive: [\n" + 
+				"		{scriptid: 'customsearch12345', name: 'Test 12345'}\n" + 
+				"	]");
 		return null;
 	}
 
