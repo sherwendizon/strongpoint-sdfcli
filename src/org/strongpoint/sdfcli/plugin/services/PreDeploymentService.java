4d3defb8763cdeb770c0160f4d40f7f68c6e7292
sherwendizon <ssadizon@gmail.com>
2019-03-13 23:16:03 +0800

    Strongpoint Eclipse Plugin v2.0: Initial commit for code refactor

---
+++ src/org/strongpoint/sdfcli/plugin/services/PreDeploymentService.java
@@ -0,0 +1,13 @@
+package org.strongpoint.sdfcli.plugin.services;
+
+public class PreDeploymentService {
+	
+	public static PreDeploymentService newInstance() {
+		return new PreDeploymentService();
+	}
+	
+	public void runPredeploymentChecks() {
+		
+	}
+
+}
